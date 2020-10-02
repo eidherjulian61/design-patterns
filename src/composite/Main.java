c7d52c721d046e3566da556f1c90eac9d04885c2
eidherjulian61 <eidherjulian61@gmail.com>
2020-10-02 18:03:35 -0500

    Composite

---
+++ src/composite/Main.java
@@ -0,0 +1,26 @@
+package composite;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Create a tree structure
+		Composite root = new Composite("root");
+		root.add(new Leaf("Leaf A"));
+		root.add(new Leaf("Leaf B"));
+
+		Composite comp = new Composite("Composite X");
+		comp.add(new Leaf("Leaf XA"));
+		comp.add(new Leaf("Leaf XB"));
+
+		root.add(comp);
+		root.add(new Leaf("Leaf C"));
+
+		// Add and remove a leaf
+		Leaf leaf = new Leaf("Leaf D");
+		root.add(leaf);
+		root.remove(leaf);
+
+		// Recursively display tree
+		root.display(1);
+	}
+}
