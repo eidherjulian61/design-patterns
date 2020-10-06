3d9bc84afb0739ced421dc0b6492ac83755ce4e1
eidherjulian61 <eidherjulian61@gmail.com>
2020-10-05 23:17:41 -0500

    Proxy

---
+++ src/proxy/Proxy.java
@@ -0,0 +1,14 @@
+package proxy;
+
+public class Proxy implements Subject {
+
+	private RealSubject realSubject;
+
+	@Override
+	public void request() {
+		if (realSubject == null) {
+			realSubject = new RealSubject();
+		}
+		realSubject.request();
+	}
+}
