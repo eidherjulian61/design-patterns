7f89e58aaed3fc6228094cad911707a88364cfe5
eidherjulian61 <eidherjulian61@gmail.com>
2020-10-07 21:37:10 -0500

    Command

---
+++ src/command/ConcreteCommand.java
@@ -0,0 +1,13 @@
+package command;
+
+public class ConcreteCommand extends Command {
+
+	public ConcreteCommand(Receiver receiver) {
+		super(receiver);
+	}
+
+	@Override
+	public void execute() {
+		receiver.action();
+	}
+}
